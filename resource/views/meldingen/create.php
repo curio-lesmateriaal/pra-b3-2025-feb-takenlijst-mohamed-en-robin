e5f6125ef6cd7244ffb4a4b52dd2579cd6733f72
robin <D312181@edu.curio.nl>
2025-04-02 11:25:36 +0200

    nieuwe taken aanmaken

--- resource/views/meldingen/create.php
+++ resource/views/meldingen/create.php
@@ -6,27 +6,32 @@
 <head>
     <title>Takenlijst | Nieuwe Taak</title>
     <?php require_once __DIR__.'/../../../head.php'; ?>
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css"> <!-- Voeg hier je CSS-bestand toe -->
 </head>
 
 <body>
 
-<?php require_once __DIR__.'/../../../head.php'; ?>
+    <header>
+        <img src="<?php echo $base_url; ?>/logo-big-v4.png" alt="Pretpark Logo">
+        <a href="<?php echo $base_url; ?>/inloggen" class="btn">Inloggen</a>
+    </header>
 
     <div class="container">
         <h1>Nieuwe taak</h1>
 
-        <form action="<?php echo $base_url; ?>/app/Http/Controllers/meldingenController.php" method="POST">
+        <form action="<?php echo $base_url; ?>/app/Http/Controllers/takencontroller.php" method="POST">
+
 
             <input type="hidden" name="action" value="create">
 
             <div class="form-group">
                 <label for="titel">Naam titel:</label>
-                <input type="text" name="titel" id="titel" class="form-input">
+                <input type="text" name="titel" id="titel" class="form-input" required>
             </div>
 
             <div class="form-group">
                 <label for="afdeling">Type</label>
-                <select name="afdeling">
+                <select name="afdeling" required>
                     <option value="personeel">Personeel</option>
                     <option value="horeca">Horeca</option>
                     <option value="techniek">Techniek</option>
@@ -45,20 +50,25 @@
 
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
-                <input type="text" name="melder" id="melder" class="form-input">
+                <input type="text" name="melder" id="melder" class="form-input" required>
             </div>
 
             <div class="form-group">
                 <label for="overig">Informatie taak:</label>
-                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
+                <textarea name="overig" id="overig" class="form-input" rows="4" required></textarea>
             </div>
 
             <div>
-                <input type="submit" value="Maak taak aan">
+                <input type="submit" value="Maak taak aan" class="btn-submit">
             </div>
 
         </form>
     </div>
+
+    <footer>
+        <p>&copy; 2025 Pretpark Takenlijst</p>
+        <p>Jowy, Mohamed, Robin</p>
+    </footer>
 
 </body>
 </html>
